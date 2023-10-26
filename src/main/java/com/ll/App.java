0950adabd6445f363f18c35c2aee10f95caea866
Hyun BeomSeok <hbs530@naver.com>
2023-10-26 13:30:21 +0900

    4단계

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -2,9 +2,13 @@ package com.ll;
 
 import java.util.Scanner;
 
-class App {
+public class App {
+
     void run() {
         System.out.println("== 명언 앱 ==");
+
+        int lastQuotationId = 0;
+
         while(true){
             System.out.print("명령) ");
 
@@ -14,13 +18,16 @@ class App {
             if (cmd.equals("종료")) {
                 break;
             } else if (cmd.equals("등록")) {
+
                 System.out.print("명언 : ");
                 String content = scanner.nextLine();
 
                 System.out.print("작가 : ");
                 String authorName = scanner.nextLine();
 
-                System.out.println("1번 명언이 등록되었습니다");
+                lastQuotationId++;
+
+                System.out.printf("%d번 명언이 등록되었습니다.\n", lastQuotationId);
             }
         }
     }
