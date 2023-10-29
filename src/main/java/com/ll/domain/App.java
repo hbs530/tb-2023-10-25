1f1ab2da8a3aa0fbdd0a8e265998635412f41a25
Hyun BeomSeok <hbs530@naver.com>
2023-10-30 07:58:45 +0900

    리팩터링, 퍼블릭 또는 프라이빗 그리고 패키지

--- src/main/java/com/ll/domain/App.java
+++ src/main/java/com/ll/domain/App.java
@@ -1,4 +1,4 @@
-package com.ll;
+package com.ll.domain;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -6,16 +6,16 @@ import java.util.Scanner;
 
 public class App {
 
-    Scanner scanner;
-    int lastQuotationId;
-    List<Quotation> quotations;
+    private Scanner scanner;
+    private int lastQuotationId;
+    private List<com.ll.domain.Quotation> quotations;
 
-    App(){
+    public App() {
         scanner = new Scanner(System.in);
         lastQuotationId = 0;
         quotations = new ArrayList<>();
     }
-    void run() {
+    public void run() {
         System.out.println("== 명언 앱 ==");
 
         while(true){
@@ -23,7 +23,7 @@ public class App {
 
             String cmd = scanner.nextLine();
 
-            Rq rq = new Rq(cmd);
+            com.ll.domain.Rq rq = new com.ll.domain.Rq(cmd);
 
             switch (rq.getAction()) {
                 case "종료":
@@ -45,7 +45,7 @@ public class App {
         }
     }
 
-    void actionWrite(){
+    private void actionWrite() {
 
         System.out.print("명언 : ");
         String content = scanner.nextLine();
@@ -56,13 +56,13 @@ public class App {
         lastQuotationId++;
         int id = lastQuotationId;
 
-        Quotation quotation = new Quotation(id, content, authorName);
+        com.ll.domain.Quotation quotation = new com.ll.domain.Quotation(id, content, authorName);
         quotations.add(quotation);
 
         System.out.printf("%d번 명언이 등록되었습니다.\n", lastQuotationId);
 
     }
-    void actionList(){
+    private void actionList() {
         System.out.println("번호 / 작가 / 명언");
 
         System.out.println("--------------------------");
@@ -71,12 +71,12 @@ public class App {
             System.out.println("등록된 명언이 없습니다.");
 
         for(int i = quotations.size() - 1; i >= 0; i-- ){
-            Quotation quotation = quotations.get(i);
+            com.ll.domain.Quotation quotation = quotations.get(i);
             System.out.printf("%d / %s / %s\n", quotation.id, quotation.authorName, quotation.content);
         }
     }
 
-    void actionRemove(Rq rq) {
+    private void actionRemove(com.ll.domain.Rq rq) {
         int id = rq.getParamAsInt("id", 0);
 
 
@@ -84,8 +84,8 @@ public class App {
             System.out.println("id를 정확히 입력해주세요.");
             return; // 함수를 끝낸다.
         }
-
-        int index = getIndexOfQuotationById(id);
+        ;
+        int index = findQuotationIndexById(id);
 
         if (index == -1) {
             System.out.printf("%d번 명언은 존재하지 않습니다.\n", id);
@@ -97,9 +97,9 @@ public class App {
         System.out.printf("%d번 명언을 삭제되었습니다.\n", id);
     }
 
-    int getIndexOfQuotationById(int id) {
+    private int findQuotationIndexById(int id) {
         for (int i = 0; i < quotations.size(); i++) {
-            Quotation quotation = quotations.get(i);
+            com.ll.domain.Quotation quotation = quotations.get(i);
 
             if (quotation.id == id) {
                 return i;
@@ -108,7 +108,7 @@ public class App {
 
         return -1;
     }
-    void actionModify(Rq rq) {
+    private void actionModify(com.ll.domain.Rq rq) {
         int id = rq.getParamAsInt("id", 0);
 
         if (id == 0) {
