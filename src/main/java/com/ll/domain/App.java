4bfed2646282efd5758f4988d567652ede6c1569
Hyun BeomSeok <hbs530@naver.com>
2023-10-30 09:01:51 +0900

    자동 목록 작성

--- src/main/java/com/ll/domain/App.java
+++ src/main/java/com/ll/domain/App.java
@@ -14,6 +14,20 @@ public class App {
         scanner = new Scanner(System.in);
         lastQuotationId = 0;
         quotations = new ArrayList<>();
+
+        initTestData();
+    }
+
+    void initTestData() {
+        for (int i = 0; i < 10; i++) {
+            lastQuotationId++;
+            int id = lastQuotationId;
+            String content = "명언 " + id;
+            String authorName = "작가 " + id;
+
+            Quotation quotation = new Quotation(id, content, authorName);
+            quotations.add(quotation);
+        }
     }
     public void run() {
         System.out.println("== 명언 앱 ==");
